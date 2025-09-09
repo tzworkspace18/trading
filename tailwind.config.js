d7c1a30d0edaa3c36a66c29a2741b99053b12f3c
tzworkspace18 <tzworkspaceb@gmail.com>
2025-09-09 20:40:52 +0530

    Fix all build issues and complete Vite + Tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx}'
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
